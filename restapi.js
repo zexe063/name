4502ba6cbefa4d4448ea9f4f7379215925adaef8
zexe <zexe063@gmail.com>
2024-08-25 12:03:13 +0530

    all are done

---
+++ restapi.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const server = express();
+const arr = []
+
+
+server.use(express.json())
+server.get("/", function(req,res){
+    res.json(arr)
+})
+
+server.get("/", (req,res)=>[
+    res.status(200).json(arr)
+])
+
+server.post("/", function(req,res){
+const  succes = {user_id:`${req.body.fullname}_${req.body.dob}` , ...req.body}
+
+
+
+
+ if(succes){
+    succes.issuccess= true
+    arr.push(succes)
+    res.status(201).json(succes)
+ }
+ 
+
+})
+
+
+server.listen("9090", function(){
+    console.log("server started")
+})
